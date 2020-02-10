57e21e8e2496d46dbd5c3197401984c1c1b2915e
likvidov <likvidya@gmail.com>
2020-02-10 20:36:06 +0300

    Сверстан первое задание

--- test1/src/js/script.js
+++ test1/src/js/script.js
@@ -1,16 +1,18 @@
 const openItem = (evt, item) => {
-    var i, tabcontent, tablinks;
-    
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
+  let i, tabcontent, tablinks;
   
-    tablinks = document.getElementsByClassName("tablinks");
+  tabcontent = document.getElementsByClassName("tab-item");
+  for (i = 0; i < tabcontent.length; i++) {      
+    tabcontent[i].style.display = "none";
+  }
+  
+    tablinks = document.getElementsByClassName("tab-link__button");
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-  
-    document.getElementById(cityName).style.display = "block";
+    
+    document.getElementsByClassName(`${item}`)[0].style.display = "block";
     evt.currentTarget.className += " active";
-}
+  }
+  
+  document.getElementById("defaultOpen").click();
