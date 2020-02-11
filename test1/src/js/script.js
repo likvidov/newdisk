eb28653a96e88400defaa49cfe8342b15640577e
likvidov <likvidya@gmail.com>
2020-02-11 18:16:04 +0300

    Last update

--- test1/src/js/script.js
+++ test1/src/js/script.js
@@ -1,22 +1,21 @@
-"use strict";
+const tabLinks = document.getElementsByClassName("tab-links");
+const tabItem = document.getElementsByClassName("tab-item");
+const tabLinksBtn = document.getElementsByClassName("tab-link__button");
 
-var tabLinks = document.getElementsByClassName("tab-links");
-var tabItem = document.getElementsByClassName("tab-item");
-var tabLinksBtn = document.getElementsByClassName("tab-link__button");
-
-var openItem = function openItem(evt, item) {
-  var i = void 0;
-
+const openItem = (evt, item) => {
+  let i;
+  
   for (i = 0; i < tabLinksBtn.length; i++) {
     tabLinksBtn[i].className = tabLinksBtn[i].className.replace(" active", "");
   }
 
-  if (document.documentElement.clientWidth > 480) for (i = 0; i < tabItem.length; i++) {
+  if(document.documentElement.clientWidth > 480)
+  for (i = 0; i < tabItem.length; i++) {      
     tabItem[i].style.display = "none";
   }
-
-  document.getElementsByClassName("" + item)[0].style.display = "block";
+  
+  document.getElementsByClassName(`${item}`)[0].style.display = "block";
   evt.currentTarget.className += " active";
-};
+}
 
 document.getElementById("defaultOpen").click();
--- test2/src/js/script.js
+++ test2/src/js/script.js
@@ -1,23 +1,22 @@
-"use strict";
+const checkbox = document.getElementsByClassName("checkbox");
+const tabItemImage = document.getElementsByClassName("tab-item__image");
+const tabItemTitle = document.getElementsByClassName("tab-item__title");
+const tabItemText = document.getElementsByClassName("tab-item__text");
 
-var checkbox = document.getElementsByClassName("checkbox");
-var tabItemImage = document.getElementsByClassName("tab-item__image");
-var tabItemTitle = document.getElementsByClassName("tab-item__title");
-var tabItemText = document.getElementsByClassName("tab-item__text");
+const titleArrayOff = ['Ветер','Дождь','Снег','Метель','Град','Гололед','Мороз'];
+const titleArrayOn = ['Сильный ветер (шквалистый)','Сильный дождь (Ливень)','Сильный снег','Сильная Метель','Крупный Град','Сильный Гололед','Сильный Мороз'];
 
-var titleArrayOff = ['Ветер', 'Дождь', 'Снег', 'Метель', 'Град', 'Гололед', 'Мороз'];
-var titleArrayOn = ['Сильный ветер (шквалистый)', 'Сильный дождь (Ливень)', 'Сильный снег', 'Сильная Метель', 'Крупный Град', 'Сильный Гололед', 'Сильный Мороз'];
-
-var switchBtn = function switchBtn(item) {
-  if (!checkbox[item].checked) {
-    tabItemImage[item].src = "img/" + (item + 1) + "_on.png";
+const switchBtn = (item) =>{
+  if(!checkbox[item].checked){
+    tabItemImage[item].src = `img/${item + 1}_on.png`;
     tabItemTitle[item].innerHTML = titleArrayOn[item];
     tabItemTitle[item].classList.add('active');
-    tabItemText[item].style.visibility = 'visible';
+    tabItemText[item].style.visibility = 'visible'
   } else {
-    tabItemImage[item].src = "img/" + (item + 1) + "_off.png";
+    tabItemImage[item].src = `img/${item + 1}_off.png`;
     tabItemTitle[item].innerHTML = titleArrayOff[item];
     tabItemTitle[item].classList.remove('active');
-    tabItemText[item].style.visibility = 'hidden';
+    tabItemText[item].style.visibility = 'hidden'
   }
-};
+}
+
