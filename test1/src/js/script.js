c528bc1fb297771199cca20d0476525b660488fa
likvidov <likvidya@gmail.com>
2020-02-11 17:41:57 +0300

    Оптимизация js  кода под ie

--- test1/src/js/script.js
+++ test1/src/js/script.js
@@ -1,21 +1,22 @@
-const tabLinks = document.getElementsByClassName("tab-links");
-const tabItem = document.getElementsByClassName("tab-item");
-const tabLinksBtn = document.getElementsByClassName("tab-link__button");
+"use strict";
 
-const openItem = (evt, item) => {
-  let i;
-  
+var tabLinks = document.getElementsByClassName("tab-links");
+var tabItem = document.getElementsByClassName("tab-item");
+var tabLinksBtn = document.getElementsByClassName("tab-link__button");
+
+var openItem = function openItem(evt, item) {
+  var i = void 0;
+
   for (i = 0; i < tabLinksBtn.length; i++) {
     tabLinksBtn[i].className = tabLinksBtn[i].className.replace(" active", "");
   }
 
-  if(document.documentElement.clientWidth > 480)
-  for (i = 0; i < tabItem.length; i++) {      
+  if (document.documentElement.clientWidth > 480) for (i = 0; i < tabItem.length; i++) {
     tabItem[i].style.display = "none";
   }
-  
-  document.getElementsByClassName(`${item}`)[0].style.display = "block";
+
+  document.getElementsByClassName("" + item)[0].style.display = "block";
   evt.currentTarget.className += " active";
-}
+};
 
 document.getElementById("defaultOpen").click();
